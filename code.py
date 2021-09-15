245590cb58b4ecd64c999d5517f9d2e926c60c08
Adam Iannazzone <adam@iannaz.zone>
2021-09-15 17:37:24 -0400

    - Reordered functions so that device launches   straight into activateHallPass() - Moved all network functions into activateHallPass() - Add additional call to activateHallPass() when device cannot   connect to SSID (if not in first 5 APs shown on scan) - Removed unused libraries (led_animation)

--- code.py
+++ code.py
@@ -1,20 +1,8 @@
 from adafruit_magtag.magtag import MagTag
-from adafruit_led_animation.color import (
-    RED,
-    GREEN,
-    BLUE,
-    CYAN,
-    WHITE,
-    OLD_LACE,
-    PURPLE,
-    MAGENTA,
-    YELLOW,
-    ORANGE,
-    PINK,
-)
 import time
 import alarm
 import board
+import random
 
 # Get secrets from private file
 try:
@@ -37,8 +25,12 @@ TIME_URL += "&fmt=%25l:%25M %25p"
 ### --- GLOBAL VARIABLES --- ###
 passActive = False
 resetList = [False, False, False, False]
-pixelColors = [RED, YELLOW, GREEN, BLUE]
 flashTime = 0.1
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+YELLOW = (255, 255, 0)
+BLUE = (0, 0, 255)
+pixelColors = [RED, YELLOW, GREEN, BLUE]
 
 magtag = MagTag(url=TIME_URL, rotation=180)
 magtag_pixels = magtag.peripherals.neopixels
@@ -59,20 +51,11 @@ def flashNeo(color):
         time.sleep(flashTime)
     magtag.peripherals.neopixel_disable = True
 
-
-# Reset MagTag text for next Hall Pass use
-def resetDisplay():
-    global magtag
-    global passActive
-    magtag.set_text("Null", index=0, auto_refresh=False)
-    magtag.set_text("Pass", index=1, auto_refresh=False)
-    passActive = False
-    magtag.refresh()
-
-
 # Initialize display
 def setupDisplay():
     global magtag
+    global passActive
+    global firstLaunch
 
     magtag.graphics.set_background("/background.bmp")
 
@@ -86,21 +69,34 @@ def setupDisplay():
         text_position=((magtag.graphics.display.width // 2), 250),
         text_anchor_point=(0.5, 0.5),
     )
-    resetDisplay()
+    magtag.set_text('', index=0, auto_refresh=False)
+    magtag.set_text('<- GO', index=1, auto_refresh=False)
+    passActive = False
+    magtag.refresh()
 
 
 # Get current time and activate Hall Pass
 def activateHallPass():
     global magtag
     global passActive
+
+    try:
+        magtag.network.connect()
+        flashNeo(GREEN)
+        print("WiFi Success")
+    except ConnectionError as e:
+        print("Retrying - ", e)
+        flashNeo(RED)
+        activateHallPass()
+
     try:
         currentTime = magtag.fetch(auto_refresh=False)
         currentTime = currentTime.strip().split(" ")
-        flashNeo(GREEN)
         for i in range(len(currentTime)):
             magtag.set_text(currentTime[i], index=i, auto_refresh=False)
         passActive = True
         magtag.refresh()
+        print('Hall Pass Created')
     except (RuntimeError, ValueError) as e:
         print("Retrying - ", e)
 
@@ -116,37 +112,23 @@ def buttonChecker():
                 magtag.peripherals.neopixel_disable = False
                 if all(resetList):
                     magtag.peripherals.neopixel_disable = True
-                    flashNeo(ORANGE)
-                    resetDisplay()
+                    flashNeo(RED)
                     magtag.peripherals.deinit()
+                    setupDisplay()
                     return
                 else:
-                    if magtag.peripherals.button_a_pressed:
-                        magtag_pixels[3] = pixelColors[3]
-                        resetList[3] = True
-                    elif magtag.peripherals.button_b_pressed:
-                        magtag_pixels[2] = pixelColors[2]
-                        resetList[2] = True
-                    elif magtag.peripherals.button_c_pressed:
-                        magtag_pixels[1] = pixelColors[1]
-                        resetList[1] = True
-                    elif magtag.peripherals.button_d_pressed:
-                        magtag_pixels[0] = pixelColors[0]
-                        resetList[0] = True
+                    for i, b in enumerate(magtag.peripherals.buttons):
+                        if not b.value:
+                            magtag_pixels[3-i] = pixelColors[3-i]
+                            resetList[3-i] = True
             else:
-                try:
-                    magtag.network.connect()
-                    print("WiFi Success")
-                    activateHallPass()
-                except ConnectionError as e:
-                    print("Retrying - ", e)
-                    flashNeo(RED)
+                activateHallPass()
 
 ### --- RUNTIME CODE --- ###
 setupDisplay()
+activateHallPass()
 buttonChecker()
 pinAlarm = [
-    alarm.pin.PinAlarm(pin=board.BUTTON_C, value=False, pull=True),
     alarm.pin.PinAlarm(pin=board.BUTTON_D, value=False, pull=True)
 ]
 alarm.exit_and_deep_sleep_until_alarms(*pinAlarm)
